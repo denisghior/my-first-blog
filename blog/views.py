203a71f835814371f04bb6c0242ab729ac900414
Denis Ghior <dxg962@student.bham.ac.uk>
2020-08-29 05:33:56 +0300

    Created delete button, added social media, modified CSS/HTML code.

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -40,3 +41,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method != 'POST':
+        post.delete()
+        return render(request, 'blog/post_list.html', {'post': post})
+    else:
+        return render(request, 'blog/post_detail.html', {'post': post})
